01bf573ac67302a666c044f8f0e8f7a198c190e9
Carl Lacharite <clacharite@blackdust.io>
2024-02-29 15:11:41 -0500

    démarrage layout

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+  <SafeAreaView style={{ flex: 1}}>
+  <View style={{ padding: 16, backgroundColor: 'green'}}>
+    <Text>search</Text>
+  </View>
+  <View style={{ flex: 1, padding: 16, backgroundColor: 'blue'}}>
+    <Text>List</Text>
+  </View>
+  </SafeAreaView>
+  </>
+ );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    Flex: 1,
   },
 });
